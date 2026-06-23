041812621f07a0ae3937b3097f4655fde2e5be77
ariful226 <arifulhaq.mu@gmail.com>
2026-06-23 11:21:47 +0600

    Add Employee and Contractor classes with functionality

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,38 @@
+public class Main{
+    public static void main(String[] args) {
+        Employee emp=new Employee("Arif");
+        emp.calculateBonus();
 
+        Contractor contractor=new Contractor("Emon");
+        contractor.completeContractWork();
+    }
+}
+
+
+class TeamMember{
+    protected String name;
+
+    public TeamMember(String name){
+        this.name = name;
+    }
+}
+
+class Employee extends TeamMember{
+    public Employee(String name){
+        super(name);
+    }
+
+    public void calculateBonus(){
+        System.out.println("Calculating standard employee bonus...");
+    }
+}
+
+class Contractor extends TeamMember{
+    public Contractor(String name){
+        super(name);
+    }
+
+    public void completeContractWork() {
+        System.out.println("Completing contract work...");
+    }
+}
