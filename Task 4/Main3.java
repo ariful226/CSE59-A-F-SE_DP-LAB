0f2a8cb88b0ef754adcab6dac7bdfd03871c1146
ariful226 <arifulhaq.mu@gmail.com>
2026-06-23 11:40:17 +0600

    Implement printer and multifunction copier classes

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
 
+        BasicPrinter printer = new BasicPrinter();
+        printer.printDocument();
+
+        MultiFunctionCopier copier = new MultiFunctionCopier();
+        copier.printDocument();
+        copier.scanDocument();
+        copier.faxDocument();
+    }
+}
+
+interface Printer {
+    void printDocument();
+}
+
+interface Scanner {
+    void scanDocument();
+}
+
+interface FaxMachine {
+    void faxDocument();
+}
+
+class MultiFunctionCopier implements Printer, Scanner, FaxMachine {
+
+    @Override
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+
+    @Override
+    public void scanDocument() {
+        System.out.println("Scanning...");
+    }
+
+    @Override
+    public void faxDocument() {
+        System.out.println("Faxing...");
+    }
+}
+
+class BasicPrinter implements Printer {
+
+    @Override
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+}
