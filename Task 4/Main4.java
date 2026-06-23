5e11f467d26d0159301220b409eace74edcabbd9
ariful226 <arifulhaq.mu@gmail.com>
2026-06-23 11:42:40 +0600

    Implement notification service with email and SMS senders

--- Task 4/Main4.java
+++ Task 4/Main4.java
@@ -1,1 +1,49 @@
+public class Main {
+    public static void main(String[] args) {
 
+        MessageSender email = new EmailSender();
+        NotificationService notification = new NotificationService(email);
+
+        notification.alertUser("Your account has been updated.");
+
+        MessageSender sms = new SMSSender();
+        NotificationService smsNotification = new NotificationService(sms);
+
+        smsNotification.alertUser("Your OTP is 1234.");
+    }
+}
+
+
+interface MessageSender {
+    void sendMessage(String message);
+}
+
+class EmailSender implements MessageSender {
+
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+
+class SMSSender implements MessageSender {
+
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+class NotificationService {
+
+    private MessageSender sender;
+
+    // Dependency Injection
+    public NotificationService(MessageSender sender) {
+        this.sender = sender;
+    }
+
+    public void alertUser(String msg) {
+        sender.sendMessage(msg);
+    }
+}
